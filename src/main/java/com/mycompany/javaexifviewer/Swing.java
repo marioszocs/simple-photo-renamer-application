a14360620c4271da4bfa3177e01905bd34cf367b
marioszocs <szocs.mario@gmail.com>
2020-09-14 21:59:36 +0200

    DELETE: old Netbeans Frame.java class

--- src/main/java/com/mycompany/javaexifviewer/Swing.java
+++ src/main/java/com/mycompany/javaexifviewer/Swing.java
@@ -3,6 +3,7 @@ package com.mycompany.javaexifviewer;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
